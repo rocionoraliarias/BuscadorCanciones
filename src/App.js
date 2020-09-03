58133ac393140510148365d0a0f9e8bc9873cced
rocionoraliarias <61668815+rocionoraliarias@users.noreply.github.com>
2020-09-02 23:32:25 -0300

    busqueda y muestra de canciones

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React,{Fragment , useState , useEffect} from 'react';
-import Form from './Components/Form'
+import Form from './Components/Form';
+import Cancion from './Components/Cancion'
+import axios from 'axios'
 
 function App() {
 //deginir el state
 
 const [busquedaLetra, guardarBusquedaLetra] = useState({});
-
+const [letra , guardarLetra] = useState('')
 useEffect(() => {
 
   if(Object.keys(busquedaLetra).length === 0 ) return;
 
-  console.log('no se ejecuta')
+const consultarAPILetra = async () => {
+const {artista , cancion} = busquedaLetra
+ const url =  `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+
+  const resultado = await axios(url);
+
+  guardarLetra(resultado.data.lyrics)
+}
+
+consultarAPILetra()
+
 },[busquedaLetra])
 
   return (
@@ -18,7 +30,18 @@ useEffect(() => {
       <Form
       guardarBusquedaLetra={guardarBusquedaLetra}
       />
-    
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            
+          </div>
+          <div className="col-md-6">
+          <Cancion
+          letra = {letra}
+          />
+          </div>
+        </div>
+      </div>
     </Fragment>
   )
 }
