5b59505649c0c4b7a849605d131811c2f3e432dd
rocionoraliarias <61668815+rocionoraliarias@users.noreply.github.com>
2020-09-02 23:05:23 -0300

    correccion errores form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React,{Fragment} from 'react';
+import React,{Fragment , useState , useEffect} from 'react';
 import Form from './Components/Form'
 
 function App() {
+//deginir el state
+
+const [busquedaLetra, guardarBusquedaLetra] = useState({});
+
+useEffect(() => {
+
+  if(Object.keys(busquedaLetra).length === 0 ) return;
+
+  console.log('no se ejecuta')
+},[busquedaLetra])
+
   return (
     <Fragment>
-      <Form/>
+      <Form
+      guardarBusquedaLetra={guardarBusquedaLetra}
+      />
     
     </Fragment>
   )
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,86 +1,91 @@
-import React,{ useState}  from 'react';
+import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
 
+    const [busqueda, guardarBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+    const [ error, guardarError] = useState(false);
 
-const [Busqueda, guardarBusqueda] = useState({
-    artista: '',
-    cancion: ''
-})
+    const { artista, cancion } = busqueda;
 
-
-const [error, guardarError] = useState(false)
-
-const { artista, cancion } = Busqueda
-//funcion a cada input para leer su contenido
-
-const acutalizarState = e => {
-
-    guardarBusqueda({
-        ...Busqueda,
-        [e.target.name] : e.target.value
-    })
-}
-
-const buscarInformacion = e => {
-    e.preventDefault();
-
-    if(artista.trim() === '' || cancion.trim() === ''){
-        guardarError(true)
+    // función a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
     }
 
-    guardarError(false)
-}
+    // consultar las apis
+    const buscarInformacion = e => {
+        e.preventDefault();
 
-    return (  
- <div className="bg-info">
-     { error ? <p className=" alert alert-danger text-center p-2">Error! todos los campos deben ser llenados</p> : null }
-    <div className="container">
-    <div className="row">
-        <form
-        onSubmit={buscarInformacion}
-        className="col card text-white bg-transparent mb-5b mt-5 pb-2"
-        >
-            <fieldset>
-                     <legend className="text-center">Buscador letras canciones</legend>
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="form-group">
-                    <label>Artista</label>
-                        <input
-                        type="text"
-                        className="form-control"
-                        name="artista"
-                        placeholder="Nombre Artista"
-                        onChange={acutalizarState}
-                        value={artista}
-                        />
-                    </div>
-                </div>
-                <div className="col-md-6">
-                <div className="form-group">
-                        <label>Cancion</label>
-                        <input
-                        type="text"
-                        className="form-control"
-                        name="Cancion"
-                        placeholder="Nombre Cancion"
-                        onChange={acutalizarState}
-                        value={cancion}
-                        />
-                    </div>
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        // Todo bien, pasar al componente principal
+
+        guardarBusquedaLetra(busqueda);
+    }
+
+    return ( 
+        <div className="bg-info">
+            { error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null}
+            <div className="container">
+                <div className="row">
+                    
+                    <form 
+                        onSubmit={buscarInformacion}
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Buscador Letras Canciones</legend>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            placeholder="Nombre Artista"
+                                            onChange={actualizarState}
+                                            value={artista}
+                                        />
+                                    </div>
+                                    
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Nombre Canción"
+                                            onChange={actualizarState}
+                                            value={cancion}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button 
+                                type="submit" 
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
+                        </fieldset>
+
+                    </form>
                 </div>
             </div>
-            <button
-            type="submit"
-            className="btn btn-primary float-right"
-            >Buscar</button>
-            </fieldset>
-        </form>
-     </div>
-    </div>
- </div>
-    );
+        </div>
+     );
 }
  
 export default Formulario;
